5eed73f9cf0cedc885c6548df422309c88562539
q-chiaki <c36630347@gmail.com>
2025-10-27 14:14:24 +0900

    remove example

--- ImageProcesser/src/main/java/com/android/imageprocesser/ImageProcessing.kt
+++ ImageProcesser/src/main/java/com/android/imageprocesser/ImageProcessing.kt
@@ -1,7 +1,23 @@
 package com.android.imageprocesser
 
-import androidx.compose.runtime.*
-import kotlinx.coroutines.*
+import androidx.compose.runtime.BroadcastFrameClock
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.Composition
+import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.Recomposer
+import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
 
 internal data class ImageContext(
     val width: Int,
@@ -30,7 +46,6 @@ public fun ImageProcessingContent(
     // ★ 再構成が完了したら処理を実行
     SideEffect {
         if (inputData != null) {
-            println("======== SideEffect: Processing image ========")
             outputData = imageProcessing.rootNode.process(inputData)
         } else {
             outputData = null
